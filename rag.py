e0ecc66170011319a47d70970e4a14b2078ba3d8
unknown <jon.sheahan@gmail.com>
2024-04-06 10:43:21 +1100

    Update rag.py

--- rag.py
+++ rag.py
@@ -53,10 +53,10 @@ class ChatPDF:
             "category_search": PromptTemplate.from_template(
                 """
                 <s> [INST] You are an assistant for document review tasks, specialising in contracts. Use the following retrieved context
-                to provide a response. If you can't find information on the {question} category, just say so and conclude the response.
-                If you find relevant information on the {question} category, concentrate on identifying and extracting all terms, clauses, and appendices related to the specified category: {question}. Your output should follow a structured and detailed format. Adhere to the instructions below for content organization and formatting:
+                to provide a response. If you can't find information on the {question} category, just say so and do not provide any further information or summary material, just end the response.
+                If you find relevant information on the {question} category, concentrate on identifying and extracting all terms, clauses, and appendices related to the specified category: {question}. Your output should follow a structured and detailed format. Adhere to the instructions below for content organization and formatting: [/INST] </s>
 
-                Summary of {question} Category
+                [INST]Summary of {question} Category
                 Begin with a Summary section focused on the {question} category. Provide an accurate summary of the key points and objectives covered under this category, including the significance of these elements to the overall contract.
                 Detailed Analysis
                 Proceed with a Detailed Analysis section where you will:
@@ -72,7 +72,7 @@ class ChatPDF:
                 Use headers and bullet points to organize the content clearly.
                 Ensure the entire response is well-structured and easy to navigate, providing a comprehensive overview and detailed breakdown of the {question} category within the contract.
                 This refined approach will enable a thorough and focused analysis of the specified category, aiding in the understanding of its terms, implications, and relevance to the contract as a whole.
-                Context: {context}
+                Context: {context}[/INST]
                 """
             ),
         }
